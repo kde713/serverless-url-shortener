bace5c42315cf264ee4ceac1de1a0630dcd9ffc4
kde713 <kde713@gmail.com>
2019-01-28 18:43:33 +0900

    Add Routers base on specification

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const serverless = require('serverless-http')
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('OK')
+app.post('/register.json', function (req, res) {
+  const originUrl = req.query.url
+  res.send(originUrl)
+})
+
+app.get('/:hashCode', function (req, res) {
+  const hashCode = req.params.hashCode
+  res.send(hashCode)
+})
+
+app.get('/:hashCode/stats', function (req, res) {
+  const hashCode = req.params.hashCode
+  res.send(`${hashCode} stats`)
 })
 
 module.exports.handler = serverless(app)
+
+app.listen(3000, function () {
+  console.log('API TEST on port 3000')
+})
