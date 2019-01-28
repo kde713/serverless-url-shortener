889177c4ae9fc76418cf50e408dd26a9edc07270
kde713 <kde713@gmail.com>
2019-01-28 19:28:38 +0900

    Implement "POST /register.json"

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
+const url = require('url')
+
 const serverless = require('serverless-http')
 const express = require('express')
+const AWS = require('aws-sdk')
+
 const app = express()
+
+import { getHash } from './utils'
+
+const TABLE_NAME = 'url-shortener'
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
 app.post('/register.json', function (req, res) {
   const originUrl = req.query.url
-  res.send(originUrl)
+  const hashedUrl = getHash(originUrl)
+
+  dynamoDb.get({
+    TableName: TABLE_NAME,
+    Key: {
+      hashCode: hashedUrl
+    }
+  }, (error, result) => {
+    if (result.Item) {
+      res.json({
+        url: url.format({
+          protocol: req.protocol,
+          host: req.get('host'),
+          pathname: `/${hashedUrl}`
+        })
+      })
+    } else {
+      dynamoDb.put({
+        TableName: TABLE_NAME,
+        Item: {
+          hashCode: hashedUrl,
+          originUrl: originUrl,
+          viewCount: 0
+        }
+      }, (error, result) => {
+        res.status(201).json({
+          url: url.format({
+            protocol: req.protocol,
+            host: req.get('host'),
+            pathname: `/${hashedUrl}`
+          })
+        })
+      })
+    }
+  })
 })
 
 app.get('/:hashCode', function (req, res) {
@@ -18,7 +61,3 @@ app.get('/:hashCode/stats', function (req, res) {
 })
 
 module.exports.handler = serverless(app)
-
-app.listen(3000, function () {
-  console.log('API TEST on port 3000')
-})
